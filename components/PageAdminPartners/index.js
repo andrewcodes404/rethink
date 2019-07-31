f903523e464c3aafb939ef552080389e9e84f3b9
Andrew <andrewcodes404@gmail.com>
2019-07-31 15:27:29 +0100

    force github to recognise letter case

---
+++ components/PageAdminPartners/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreatePartner from './CreatePartner'
+import UpdatePartner from './UpdatePartner'
+
+const Index = () => (
+    <div className="content-wrapper">
+        <CreatePartner />
+        <UpdatePartner />
+    </div>
+)
+
+export default Index
