8d3c3fec2e10dbedff0b1759842fc9aa8bd3ba85
Andrew <andrewcodes404@gmail.com>
2019-12-02 11:04:11 +0000

    test

--- components/PageAdminSessions/index.js
+++ components/PageAdminSessions/index.js
@@ -5,7 +5,7 @@ class PageAdminSessions extends React.Component {
     render() {
         return (
             <div className="text-content-title-wrapper">
-                <h2 data-aos="my-anim">Sesions</h2>
+                <h2 data-aos="my-anim">Sessions</h2>
 
                 <div className="text-content">
                     <CreateSession />
