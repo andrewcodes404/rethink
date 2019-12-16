fdad38d84c0739de207dda91a7a168284773c449
Andrew <andrewcodes404@gmail.com>
2019-12-16 10:05:17 +0000

    add media cards to partners page

--- pages/_document.js
+++ pages/_document.js
@@ -8,9 +8,7 @@ export default class MyDocument extends Document {
         const isProduction = process.env.NODE_ENV === 'production'
         // renders the page and check for any styles and "collects" them
         const sheet = new ServerStyleSheet()
-        const page = renderPage(App => props =>
-            sheet.collectStyles(<App {...props} />)
-        )
+        const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
         // compile the styles and dump them on the page
         const styleTags = sheet.getStyleElement()
         return { ...page, styleTags, isProduction }
@@ -38,14 +36,9 @@ export default class MyDocument extends Document {
                     {/* We only want to add the scripts if in production */}
                     {isProduction && (
                         <Fragment>
-                            <script
-                                async
-                                src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXX-X"
-                            />
+                            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXX-X" />
                             {/* We call the function above to inject the contents of the script tag */}
-                            <script
-                                dangerouslySetInnerHTML={this.setGoogleTags()}
-                            />
+                            <script dangerouslySetInnerHTML={this.setGoogleTags()} />
                         </Fragment>
                     )}
                 </body>
