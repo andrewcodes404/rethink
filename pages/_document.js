214486d6c1c3396980531eb324a996968ebfa63e
andrew <andrew.dickens@manifesto.co.uk>
2020-05-06 18:44:46 +0100

    move GTM scripts to _document

--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,18 @@ export default class MyDocument extends Document {
         }
     }
 
+    setGoogleTags2() {
+        return {
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-584PS8B');
+            `
+        }
+    }
+
 
     render() {
         const { isProduction } = this.props
@@ -32,6 +44,11 @@ export default class MyDocument extends Document {
             <html>
                 <Head>{this.props.styleTags}</Head>
                 <body>
+                    {/* <!-- Google Tag Manager (noscript) --> */}
+                    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-584PS8B"
+                        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+                    {/* <!-- End Google Tag Manager (noscript) --> */}
+
                     <Main />
                     <NextScript />
 
@@ -41,11 +58,7 @@ export default class MyDocument extends Document {
                             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXX-X" />
                             {/* We call the function above to inject the contents of the script tag */}
                             <script dangerouslySetInnerHTML={this.setGoogleTags()} />
-
-                            {/* <!-- Google Tag Manager (noscript) --> */}
-                            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-584PS8B"
-                                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-                            {/* <!-- End Google Tag Manager (noscript) --> */}
+                            <script dangerouslySetInnerHTML={this.setGoogleTags2()} />
                         </Fragment>
                     )}
                 </body>
